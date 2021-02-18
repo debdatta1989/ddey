f6c8a37503c8b93763e174e8902fd31aefe468b5
Debdatta <debdatta.dey-ext@kering.com>
2021-02-18 05:43:27 +0100

    changed pom

--- src/main/java/steps/navigationCrawler.java
+++ src/main/java/steps/navigationCrawler.java
@@ -25,14 +25,14 @@ public class navigationCrawler {
     }
 
 
-        @When("I search the {} in the search box and enter")
-        public void searchTheWord(String key){
+    @When("I search the {} in the search box and enter")
+    public void searchTheWord(String key){
         driver.findElement(By.cssSelector("body > div.L3eUgb > " +
                 "div.o3j99.ikrT4e.om7nvf > form > div:nth-child(2) > div.A8SBwf > " +
                 "div.RNNXgb > div > div.a4bIc > input")).sendKeys(key,ENTER);
 
 
-        }
+    }
 
     @Then("I got the result and validate the search result")
     public void verifyResult(){
